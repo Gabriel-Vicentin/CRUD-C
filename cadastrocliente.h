0e7b3b30b61b5ee41eac936678b13b4a4f0dbabc
Gabriel-Vicentin <gabriel.vicentin.bonfim@gmail.com>
2024-06-25 14:01:57 -0300

    Menu principal

---
+++ cadastrocliente.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct user { 
+    char nome[70];
+    int id;
+};
+
+void cadastrocliente () {
+    struct user usuario[5];
+    int op, i = 0;
+}
