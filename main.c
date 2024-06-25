9c462b064233cdbd9cc9bee16d8b26966e4b8ef0
Gabriel-Vicentin <gabriel.vicentin.bonfim@gmail.com>
2024-06-25 13:45:03 -0300

    Menu main

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cadastro_produto.h"
+#include "cadastrocliente.h"
+
+int main() {
+    int op, cont;
+    do{
+    printf("escolha uma opcao \n");
+    printf("1 - cadastro cliente \n");
+    printf("2 - Cadastro produto \n");
+        scanf("%i", &op);
+
+        switch (op){
+
+        case 1:
+            cadastrocliente();
+            break;
+
+        case 2:
+            menu();
+            break;
+
+        default:
+            printf("Opcao incorreta\n");
+            break;
+        }
+        printf("Deseja continuar?\n");
+        printf("1 - Continuar\n");
+        printf("0 - Parar\n");
+            scanf("%i", &cont);
+    }while (cont == 1);
+return 0;
+}
